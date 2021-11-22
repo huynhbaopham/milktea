7a3ae7031115a04300d28462cd5c12ffb07704a6
Bao Pham <huynhbaopham@gmail.com>
2021-11-22 13:17:04 -0900

    logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,36 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#F6EAD9`,
       marginBottom: `1.45rem`,
     }}
   >
+    <Link
+      to="/"
+      style={{
+        color: `white`,
+        textDecoration: `none`,
+      }}
+    >
+      <StaticImage>
+        src="../images/XFTLogo.png" width={150}
+        quality={95}
+        formats={["auto", "webp", "avif"]}
+        alt="Logo" style={{ marginBottom: `1.45rem` }}
+      </StaticImage>
+    </Link>
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+    ></div>
   </header>
 )
 
