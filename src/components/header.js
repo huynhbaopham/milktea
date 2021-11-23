6cf6c18c89d63e0d06e15b7968c41cf755f4cb42
Bao Pham <huynhbaopham@gmail.com>
2021-11-22 16:48:16 -0900

    navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,46 +1,10 @@
 import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import NavBar from "./components/Navbar"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `#F6EAD9`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <Link
-      to="/"
-      style={{
-        color: `white`,
-        textDecoration: `none`,
-      }}
-    >
-      <StaticImage
-        src="../images/XFTLogo.png"
-        width={150}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt="Logo"
-      />
-    </Link>
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    ></div>
+  <header>
+    <NavBar />
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
