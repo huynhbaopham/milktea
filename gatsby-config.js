0f47e3bea5d9bc8ba1da212bfde9dbe189bbbcb5
Bao Pham <huynhbaopham@gmail.com>
2021-12-01 21:53:52 -0900

    hi

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-smoothscroll`,
-    `gatsby-plugin-fontawesome-css`,
+    `react-icons`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
